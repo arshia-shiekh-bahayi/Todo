485edde23f747f5d8454afcc46dff92f724827f8
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-06-16 13:43:54 +0430

    Update paginations.py

--- core/Todo/api/v1/paginations.py
+++ core/Todo/api/v1/paginations.py
@@ -1,11 +1,17 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 class DefaultPagination(PageNumberPagination):
     page_size = 10
 
     def get_paginated_response(self, data):
+        filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+        filterset_fields = {'author': ["exact"],
+                        'title': ["exact"], 'status': ["exact"]}
+        search_fields = ['title', 'content']
+        ordering_fields = ['created_date', ]
         return Response(
             {
                 "links": {
@@ -15,5 +21,11 @@ class DefaultPagination(PageNumberPagination):
                 "total_objects": self.page.paginator.count,
                 "total_pages": self.page.paginator.num_pages,
                 "results": data,
+                
+                "filterset_fields": filterset_fields,
+                "search_fields": search_fields,
+                "ordering_fields": ordering_fields,
+                "results": data,
             }
         )
+    
